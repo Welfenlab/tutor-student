dede4d4876dd2b03fa2eed8e99d584c9bfee5c91
Maximilian Klein <dasmaxl@gmail.com>
2015-09-06 23:26:26 +0200

    (fix) First tests are now working.

--- test/rest.js
+++ test/rest.js
@@ -2,13 +2,13 @@
 chai = require("chai");
 chai.should();
 
-var config = require("cson").load("config.cson");
+var config = require("cson").load("./test/fixtures/config.cson");
 var request = require("request");
 var TutorServer = require("@tutor/server");
 
 
 var memDB = require("@tutor/memory-database")();
-memDB.Restore("./fixtrues/db.json");
+memDB.Restore("./test/fixtures/db.json");
 restAPI = require("../src/rest")(memDB);
 var server = TutorServer(config);
 
@@ -25,15 +25,28 @@ var doRequest = function(method, path, fn){
         'Content-Type': 'MyContentType',
         'Custom-Header': 'Custom Value'
     }
-  }, fn);
+  }, function(err, res, body){
+    fn(err, res, JSON.parse(body));
+  });
 }
 
 describe("Student REST API", function(){
   it("should return all exercises",function(done){
     doRequest("GET","/exercises",
       function(err, res, body){
-        JSON.parse(body).length.should.equal(2);
+        body.should.have.length(2);
         res.statusCode.should.equal(200);
+        done();
+      });
+  });
+  it("list of exercises should only contain task ids",function(done){
+    doRequest("GET","/exercises",
+      function(err, res, body){
+        body.forEach(function(ex){
+          ex.tasks.forEach(function(t){
+            (typeof(t)).should.equal("string");
+          });
+        });
         done();
       });
   });
