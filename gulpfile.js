a1ced6efcdd9922ce3215d5171b901a96ec5f2a6
Maximilian Klein <dasmaxl@gmail.com>
2015-09-12 13:13:39 +0200

    (feature) Separate most vendor packages.

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,62 @@ var less        = require('gulp-less');
 var concat      = require('gulp-concat');
 var watch       = require('gulp-watch');
 
+var libs = [
+  "brace",
+  "knockout",
+  "knockout-mapping",
+  "d3",
+  "esprima",
+  "dagre-d3",
+  "markdown-it",
+  "markdown-it-math",
+  "async",
+  "ascii2mathml",
+  "i18next-ko",
+  "graphlib-dot",
+  "lodash"
+];
+
 // browserify bundle for direct browser use.
-gulp.task("bundle", function(){
+gulp.task("app", function(){
   bundler = browserify('./app/scripts/tutor.coffee',
     {
       transform: ['coffeeify','brfs'],
 //      standalone: 'tutor',
       extensions: ['.coffee'],
       debug: false,
-      noParse: ['knockout-mapping']
+      noParse: ['knockout-mapping'],
+      fullPaths: true
     });
+  libs.forEach(function(lib) {
+    bundler.external(lib);
+  });
 
   return bundler.bundle()
     .on('error', function(err){ console.log(err.message); this.emit('end');})
     .pipe(source('tutor.js'))
     .pipe(gulp.dest('build'));
 });
+
+gulp.task("build-libs", function(){
+  bundler = browserify({
+      transform: ['coffeeify','brfs'],
+  //      standalone: 'tutor',
+      extensions: ['.coffee'],
+      debug: false,
+      noParse: ['knockout-mapping']
+    });
+  libs.forEach(function(lib) {
+    bundler.require(lib);
+  });
+
+  return bundler.bundle()
+    .on('error', function(err){ console.log(err.message); this.emit('end');})
+    .pipe(source('vendor.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task("bundle", ["build-libs", "app"]);
 
 gulp.task("styles", function(){
   gulp.src("./app/styles/*.less")
