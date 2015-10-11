971642360dce4e0269024889c3635c7efed4bc95
Maximilian Klein <dasmaxl@gmail.com>
2015-10-11 18:28:32 +0200

    (feature) Configure DB via environment variables in production.

--- src/production.js
+++ src/production.js
@@ -1,16 +1,29 @@
 
 // initialize the production environment
-module.exports = function(app, config){
+module.exports = function(config){
   console.log("production environment");
-  var rethinkDB = require("@tutor/rethinkdb-database")(config);
+  console.log("... waiting 10s for RethinkDB");
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve();
+    },10000);
+  }).then(function(){
+    config.database.host = process.env.RETHINKDB_PORT_28015_TCP_ADDR;
+    config.database.port = parseInt(process.env.RETHINKDB_PORT_28015_TCP_PORT);
+    console.log("database connection: " + config.database.host + ":" + config.database.port);
+    var rethinkDB = require("@tutor/rethinkdb-database")(config);
 
 
-  config.modules = []
+    config.modules = []
 
-  return rethinkDB.then(function(DB){
-    restAPI = require("./rest")(DB);
-    config.modules.push(require("@tutor/saml")(DB.Users.create, DB.Users.exists,
-        Promise.resolve() ));
-    resolve(restAPI);
+    return rethinkDB.then(function(DB){
+      restAPI = require("./rest")(DB);
+      config.modules.push(require("@tutor/saml")(DB.Users.create, DB.Users.exists,
+          Promise.resolve() ));
+      config.modules.push(function(app, config){
+        app.use(express.static('./build'));
+      });
+      return {api: restAPI, db: DB};
+    });
   });
 }
