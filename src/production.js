cc4c5668f092d0c1e90de38653b2ba08f67ba935
Maximilian Klein <dasmaxl@gmail.com>
2015-12-20 17:35:26 +0100

    Version 0.4.0
    - Cleaner package. Less produciton dependencies
    - Configuration over environment variables in production mode
    - Updated Autentification package.

--- src/production.js
+++ src/production.js
@@ -5,10 +5,19 @@ var express = require("express");
 module.exports = function(config){
   console.log("production environment");
   console.log("... waiting 10s for RethinkDB");
+  
+  config.domainname = process.env.TUTOR_DOMAIN_NAME
   config.database.host = process.env.RETHINKDB_PORT_28015_TCP_ADDR;
   config.database.port = parseInt(process.env.RETHINKDB_PORT_28015_TCP_PORT);
   config.sharejs.rethinkdb.host = config.database.host;
   config.sharejs.rethinkdb.port = config.database.port;
+  config.session = config.session | {}
+  config.session.secret = process.env.TUTOR_SESSION_SECRET
+  config.saml.privateKey = process.env.TUTOR_SAML_KEY;
+  config.saml.certificate = process.env.TUTOR_SAML_CERT
+  config.saml.idpLoginUrl = process.env.TUTOR_IDP_LOGIN_URL
+  config.saml.idpCertificate = process.env.TUTOR_IDP_CERT
+  
   console.log("database connection: " + config.database.host + ":" + config.database.port);
   return new Promise(function(resolve){
     setTimeout(function(){
